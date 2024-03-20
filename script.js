50b3eed8875e99c674a1173d4069b3e0c5742718
Rehan Faryad <rehanfaryad2802@gmail.com>
2024-03-20 10:00:26 +0500

    link-added

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let changeColor = (e) => {
-  let target = document.querySelector(".note");
   let color = document.querySelectorAll('[class^="bg"]');
   color.forEach((e) => {
     e.addEventListener("click", (j) => {
       let text = j.target.closest(".note");
       text.querySelector(".textarea").style.backgroundColor = j.target.value;
+      text.querySelector(".header").style.backgroundColor = j.target.value;
+      text.style.backgroundColor = j.target.value;
     });
   });
 };
@@ -16,45 +17,62 @@ let insert = (e) => {
   tag.classList.add("m5");
   tag.classList.add("f25");
   let html = `
-    <div class="header f jcsb p">
+  <div class="header f jcsb aic p">
     <div class="back p f">
-    <button class="bg1" value="#e5ff00"></button>
-    <button class="bg2" value="#77ff00"></button>
-    <button class="bg3" value="#05ebd7"></button>
-    <button class="bg4" value="#ffae00"></button>
+        <button class="bg1" value="#e5ff00"></button>
+        <button class="bg2" value="#77ff00"></button>
+        <button class="bg3" value="#05ebd7"></button>
+        <button class="bg4" value="#ffae00"></button>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x cp close"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+    <div class="links f aic jcsb">
+        <input type='text' class="anker">
+        <p>Go</p>
+    </div>
+    <div class="controler f aic jcsb">
+        <p class='aTag'><u class="a">a</u> 
+        </p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x cp close">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
+        </svg>
+    </div>
     </div>
     <div class="contant">
-    <textarea name="" class="textarea" cols="30" rows="10"></textarea>
+    <div class="all_links"></div>
+        <textarea name="" class="textarea" spellcheck="false" cols="30" rows="10"></textarea>
     </div>
-    `;
+
+  `;
   tag.innerHTML = html;
   let all_notes = document.querySelector(".all_notes");
   all_notes.appendChild(tag);
 };
 document.querySelector(".creat").addEventListener("click", (e) => {
   insert();
+  aTag();
+  linker();
+  linker2();
   changeColor();
   close();
+  removeF();
 });
 document.querySelector(".save").addEventListener("click", (e) => {
-    save();
+  save();
 });
 let save = (e) => {
-    let text = document.querySelector(".all_notes").innerHTML;
-    let textarea = document.querySelectorAll("textarea");
-    let content = [];
-    for (let j = 0; j < textarea.length; j++) {
-      const element = textarea[j].value;
-      content.push(element);
-    }
-    localStorage.setItem("text", text);
-    localStorage.setItem("content", content);
-    document.querySelector(".save").textContent = "Saved!";
-    setTimeout(() => {
-        document.querySelector(".save").textContent = "Save";
-    }, 1500);
+  let text = document.querySelector(".all_notes").innerHTML;
+  let textarea = document.querySelectorAll("textarea");
+  let content = [];
+  for (let j = 0; j < textarea.length; j++) {
+    const element = textarea[j].value;
+    content.push(element);
+  }
+  localStorage.setItem("text", text);
+  localStorage.setItem("content", content);
+  document.querySelector(".save").textContent = "Saved!";
+  setTimeout(() => {
+    document.querySelector(".save").textContent = "Save";
+  }, 1500);
 };
 window.addEventListener("load", (e) => {
   let text = localStorage.getItem("text");
@@ -70,6 +88,10 @@ window.addEventListener("load", (e) => {
   }
   changeColor();
   close();
+  aTag();
+  linker();
+  linker2();
+  removeF();
 });
 let close = () => {
   document.querySelectorAll(".close").forEach((e) => {
@@ -81,7 +103,101 @@ let close = () => {
 };
 document.addEventListener("keydown", (e) => {
   if (e.ctrlKey && e.key == "s") {
-    e.preventDefault()
+    e.preventDefault();
     save();
   }
 });
+let linker = (e) => {
+  let links = document.querySelectorAll(".anker");
+  links.forEach((e) => {
+    e.addEventListener("keyup", (j) => {
+      if (j.key === "Enter" || j.keyCode === 13) {
+        let a = j.target.closest(".note");
+        let link = j.target.value;
+        let refre = link.split(".");
+        let c = a.querySelector(".all_links");
+        if (refre[1] == undefined) {
+          refre[1] = link;
+          c.innerHTML += `
+          <div class="Cont f aic jcsb p5">
+            <a href="https://www.${link}.com" class="a under" target="_blank">${refre[1]}</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"  viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  class="lucide lucide-x cp remove">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
+          </svg>
+          </div>
+          `;
+          j.target.value = "";
+        } else {
+          c.innerHTML += `
+          <div class="Cont f aic jcsb p5">
+            <a href="${link}" class="a under" target="_blank">${refre[1]}</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"  viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  class="lucide lucide-x cp remove">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
+          </svg>
+          </div>
+            `;
+        }
+        // console.log(refre);
+        j.target.value = "";
+        removeF();
+      }
+    });
+  });
+};
+let linker2 = (e) => {
+  let links = document.querySelectorAll(".links p");
+  links.forEach((e) => {
+    e.addEventListener("click", (j) => {
+      let a = j.target.closest(".note");
+      let b = a.querySelector("input");
+      let link = b.value;
+      let refre = link.split(".");
+      let c = a.querySelector(".all_links");
+      if (refre[1] == undefined && refre[1] != "") {
+        refre[1] = link;
+        c.innerHTML += `
+          <div class="Cont f aic jcsb p5">
+            <a href="https://www.${link}.com" class="a under" target="_blank">${refre[1]}</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"  viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  class="lucide lucide-x cp remove">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
+          </svg>
+          </div>
+            `;
+        b.value = "";
+      } else {
+        c.innerHTML += `
+          <div class="Cont f aic jcsb p5">
+            <a href="${link}" class="a under" target="_blank">${refre[1]}</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"  viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  class="lucide lucide-x cp remove">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
+          </svg>
+          </div>
+            `;
+      }
+      b.value = "";
+      // console.log(refre);
+      removeF();
+    });
+  });
+};
+let aTag = (e) => {
+  document.querySelectorAll(".aTag").forEach((e) => {
+    e.addEventListener("click", (j) => {
+      let a = j.target.closest(".note");
+      let b = a.querySelector(".links");
+      b.classList.toggle("hide");
+    });
+  });
+};
+let removeF = (e) => {
+  document.querySelectorAll(".remove").forEach((e) => {
+    e.addEventListener("click", (j) => {
+      j.target.closest(".Cont").remove();
+      // .remove();
+    });
+  });
+};
